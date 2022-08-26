62266fc02b1476e39ed0b5a4d1542ca512dd61b3
Alexander Du <111344246+Alexduuu@users.noreply.github.com>
2022-08-26 16:15:06 +0800

    Create Weapon.java

---
+++ Exercise_1/Weapon.java
@@ -0,0 +1,15 @@
+public class Weapon {
+    //Weapon attributes
+    int damage;
+    int weight;
+    String rarity;
+    String description;
+    String name;
+
+    //Weapon method
+    public void weaponDescription() {
+        System.out.println("My Weapon is " + name + description);
+
+    }
+   
+}
