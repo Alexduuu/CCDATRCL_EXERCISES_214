69355dc121c4acb99ca18d57952d66fd7641179f
Alexander Du <111344246+Alexduuu@users.noreply.github.com>
2022-09-06 15:13:28 +0800

    Create Stack.java

---
+++ Exercise_3/Stack.java
@@ -0,0 +1,63 @@
+public class Stack {
+
+    
+    String arr[]; 
+    int top;
+    int capacity;
+
+    
+    Stack(int size) {
+        arr = new String[size];
+        capacity = size;
+        top = -1;
+    }
+
+    public void push(String data) {
+        if (isFull()) {
+            System.out.println("Stack is Full!");
+            System.exit(1);
+        }
+
+        System.out.println("Inserting " + data);
+        arr[++top] = data;
+    }
+
+    public String pop() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty!");
+            System.exit(1);
+        }
+
+        System.out.println("Removing " + arr[top]);
+
+        return arr[top--];
+    }
+
+    public int size() {
+        return top + 1;
+    }
+
+    public Boolean isEmpty() {
+        return top == -1;
+    }
+
+    public Boolean isFull() {
+        return top == capacity - 1;
+    }
+
+    public String peek() {
+
+        if (isEmpty()) {
+            System.out.println("The stack is empty");
+            System.exit(-1);
+        }
+
+        return arr[top];
+    }
+
+    public void printStack() {
+        for (int i = top; i >= 0; i--) {
+            System.out.println(arr[i]);
+        }
+    }
+}
